278dfbd53ef4ffa5e43c637192c90203bc1d2997
Keon Min <keonwoong.min@sjsu.edu>
2019-08-22 14:48:07 -0700

    Update TestJuly28SecondVersion.ino

--- TestJuly28SecondVersion.ino
+++ TestJuly28SecondVersion.ino
@@ -1,5 +1,11 @@
 //4 time interval
-
+/**
+ * This class measures the tempo as you tap the drum
+ * and calculates the BPM and shows up on the 7 segment displays as a 3 digits
+ * it measure with 4 time intervals
+ * @author KeonwoongMin
+ *
+ */
 const int a = 2;
 const int b = 3;
 const int c = 4;
